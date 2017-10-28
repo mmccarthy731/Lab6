c7f597414fe882022b846f94c88bf533aa77088d
Michael R. McCarthy <mmccarthy731@yahoo.com>
2017-10-27 22:15:39 -0400

    created a seperate method for translating, cleaned up some code

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -8,38 +8,22 @@ namespace Lab6
 {
     class Program
     {
+        static char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        static char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the Pig Latin Translator!\n\nThis program takes a word that you enter and translates it into Pig Latin!\n");
+            Console.WriteLine("This is the Pig Latin Translator!\nThis program takes a sentence that you enter and translates it into Pig Latin!");
             string name = GetName("Please enter your name: ");
-
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            Console.Write($"Hello, {name}! ");
 
             bool repeat = true;
             while (repeat)
             {
-                string[] userInput = GetWord("Please enter a sentence to be translated into Pig Latin: ");
-                Console.Write("\nYour entry translated to Pig Latin is: ");
-                for (int i = 0; i < userInput.Count(); i++)
-                {
-                    string word = userInput[i];
-                    int position = word.ToLower().IndexOfAny(vowels);
-            
-                    char[] arrayOne = word.ToCharArray(0, position);
-                    string preString = new string(arrayOne);
-                    string postString = word.Substring(position);
-                    if (position == 0)
-                    {
-                        string result = string.Concat(word, "way");
-                        Console.Write(result+" ");
-                    }
-                    else
-                    {
-                        string result = string.Concat(postString, preString, "ay");
-                        Console.Write(result + " ");
-                    }
-                }
-                repeat = DoAgain($"\n\nWould you like to translate another sentence to Pig Latin, {name}? (Y or N): ");
+                string[] userInput = GetInput("Please enter a sentence to be translated into Pig Latin: ");
+                string pigLatin = Translate(userInput);
+                Console.WriteLine($"\nYour entry translated to Pig Latin is:\n\n{pigLatin}\n");
+                repeat = DoAgain($"Would you like to translate another sentence to Pig Latin, {name}? (Y or N): ");
             }
             Console.WriteLine("Thank you for using the Pig Latin Translator!\n\nGoodbye!");
         }
@@ -47,19 +31,64 @@ namespace Lab6
         private static string GetName(string prompt)
         {
             Console.Write(prompt);
-            string name = Console.ReadLine();
-            Console.Write($"\nHello {name}! ");
-            return name;
+            return Console.ReadLine();
         }
 
-        private static string[] GetWord(string prompt)
+        private static string[] GetInput(string prompt)
         {
-            Console.Write(prompt);
+            Console.WriteLine($"{prompt}\n");
             string input = Console.ReadLine().Trim();
             string[] arrayOne = input.Split(null as string[], StringSplitOptions.RemoveEmptyEntries);
             return arrayOne;
         }
 
+        private static bool IsAlpha(string input)
+        {
+            char[] letters = input.ToCharArray();
+            foreach(char letter in letters)
+            {
+                if (!char.IsLetter(letter))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static string Translate(string[] userInput)
+        {
+            int length = userInput.Count();
+            string[] resultArray = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                string word = userInput[i];
+                int position = word.ToLower().IndexOfAny(vowels);
+                if (position < 0)
+                {
+                    resultArray[i] = word;
+                }
+                else
+                {
+                    char[] arrayOne = word.ToCharArray(0, position);
+                    string preString = new string(arrayOne);
+                    string postString = word.Substring(position);
+                    if (!IsAlpha(word) && !word.Contains("'"))
+                    {
+                        resultArray[i] = word;
+                    }
+                    else if (position == 0)
+                    {
+                        resultArray[i] = string.Concat(word, "way");
+                    }
+                    else
+                    {
+                        resultArray[i] = string.Concat(postString, preString, "ay");
+                    }
+                }
+            }
+            string resultString = string.Join(" ", resultArray);
+            return resultString;
+        }
 
         private static bool DoAgain(string prompt)
         {
